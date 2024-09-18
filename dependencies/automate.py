7ddbff8bf4c8e59a1b8155960c64d935ab400ae5
Swoyam Siddharth Nayak <me@swoyam.in>
2024-09-18 13:24:03 +0530

    added multi threading in mail sending

--- dependencies/automate.py
+++ dependencies/automate.py
@@ -1,6 +1,7 @@
 from dependencies import mongo
 from dependencies import email
 from models.opportunities import Job
+from concurrent.futures import ThreadPoolExecutor
 
 subscribers = mongo.db["subscribers"]
 
@@ -8,5 +9,8 @@ async def sendBulkMails(job: Job):
     subscriberList = subscribers.find(
         {}
     )
-    for i in subscriberList:
-        email.send_opportunity(i["email"], job)
+
+    with ThreadPoolExecutor(max_workers=200) as executor:
+        futures = [executor.submit(email.send_opportunity, i["email"], job) for i in subscriberList]
+        for future in futures:
+            future.result()
